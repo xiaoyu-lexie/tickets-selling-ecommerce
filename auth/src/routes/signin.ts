bd682bb50d402eb148dd31f27e5aea3fc5b7ead2
Lexie Chen <xiaoyuchen.uncc@gmail.com>
2022-02-28 11:40:34 -0800

    add sign in validation

--- auth/src/routes/signin.ts
+++ auth/src/routes/signin.ts
@@ -1,9 +1,20 @@
 import express from 'express'
+import {body} from 'express-validator'
 
 const router = express.Router();
 // app.use(json());
 
-router.post('/api/users/signin', (req, res)=>{
+router.post('/api/users/signin',
+[
+  body('email')
+    .isEmail()
+    .withMessage('Email must be valid'),
+  body('password')
+    .trim()
+    .notEmpty()
+    .withMessage('You must supply a valid password')
+],
+(req, res)=>{
   res.send('Hi there!')
 })
 
