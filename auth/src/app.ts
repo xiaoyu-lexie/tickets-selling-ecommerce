7ca8a28321b68af39e1356c0c708543bc066fc0e
Lexie Chen <xiaoyuchen.uncc@gmail.com>
2022-03-11 11:45:28 -0800

    seperate index for supertest

---
+++ auth/src/app.ts
@@ -0,0 +1,38 @@
+import express from 'express'
+require('express-async-errors');
+import {json} from 'body-parser'
+import cookieSession from 'cookie-session';
+
+import {currentUserRouter} from './routes/current-user'
+import { signinRouter } from './routes/signin';
+import { signoutRouter } from './routes/signout';
+import { signupRouter } from './routes/signup';
+import {errorHandler} from './middleware/error-handler'
+import {NotFoundError} from './errors/not-found-error'
+
+const app = express();
+// traffic is being proximate to our app through ingress-nginx, so this is to make Express trust proxy
+app.set('trust proxy', true)
+app.use(json());
+app.use(
+  cookieSession({
+    // to disable encryption on this cookie
+    signed: false,
+    // to require that cookie will only be used if a user is visiting our app over an HTTP connection
+    secure: true
+  })
+)
+
+app.use(currentUserRouter);
+app.use(signinRouter);
+app.use(signoutRouter);
+app.use(signupRouter);
+
+// put this before errorHandler
+app.all('*', async(req, res) => { // "all" includes all methods (get, post....)
+  throw new NotFoundError() ;
+});
+
+app.use(errorHandler);
+
+export {app};
--- auth/src/index.ts
+++ auth/src/index.ts
@@ -1,40 +1,6 @@
-import express from 'express'
-require('express-async-errors');
-import {json} from 'body-parser'
 import mongoose from 'mongoose';
-import cookieSession from 'cookie-session';
 
-import {currentUserRouter} from './routes/current-user'
-import { signinRouter } from './routes/signin';
-import { signoutRouter } from './routes/signout';
-import { signupRouter } from './routes/signup';
-import {errorHandler} from './middleware/error-handler'
-import {NotFoundError} from './errors/not-found-error'
-
-const app = express();
-// traffic is being proximate to our app through ingress-nginx, so this is to make Express trust proxy
-app.set('trust proxy', true)
-app.use(json());
-app.use(
-  cookieSession({
-    // to disable encryption on this cookie
-    signed: false,
-    // to require that cookie will only be used if a user is visiting our app over an HTTP connection
-    secure: true
-  })
-)
-
-app.use(currentUserRouter);
-app.use(signinRouter);
-app.use(signoutRouter);
-app.use(signupRouter);
-
-// put this before errorHandler
-app.all('*', async(req, res) => { // "all" includes all methods (get, post....)
-  throw new NotFoundError() ;
-});
-
-app.use(errorHandler);
+import {app} from './app';
 
 const start = async() => {
   if (!process.env.JWT_KEY) { // we putting the check here is to know the error immediately when we start to deploy code
