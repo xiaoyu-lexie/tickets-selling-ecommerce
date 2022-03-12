44d3564684610c23f2197e5e7505df46cd09e502
Lexie Chen <xiaoyuchen.uncc@gmail.com>
2022-03-11 16:46:37 -0800

    test environment setup

---
+++ auth/src/test/setup.ts
@@ -0,0 +1,28 @@
+import {MongoMemoryServer} from 'mongodb-memory-server';
+import mongoose from 'mongoose';
+import { app } from '../app';
+
+let mongo: any;
+
+// to hook up MongoMemoryServer
+beforeAll(async () => {
+  mongo = new MongoMemoryServer();
+  const mongoUri = await mongo.getUri();
+
+  await mongoose.connect(mongoUri);
+});
+
+// everytime we run test, we clean up old data
+beforeEach(async () => {
+  const collections = await mongoose.connection.db.collections();
+
+  for (let collection of collections) {
+    await collection.deleteMany({});
+  }
+});
+
+// after all tests, we want to close the connection
+afterAll(async () => {
+  await mongo.stop();
+  await mongoose.connection.close();
+})
