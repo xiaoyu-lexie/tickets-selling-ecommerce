53462c5c01731a63f4a918e38040de9e43f9606f
Lexie Chen <xiaoyuchen.uncc@gmail.com>
2022-03-20 20:23:35 -0700

    fetch auth data when server side rendering process

--- client/pages/index.js
+++ client/pages/index.js
@@ -1,3 +1,16 @@
-export default () => {
+import axios from 'axios';
+
+const LandingPage = ({currentuser}) => {
+  console.log(currentuser);
+  // axios.get('/api/users/currentuser')
+
   return <h1>Landing page</h1>
 };
+
+LandingPage.getInitialProps = async () => {
+  const response = await axios.get('/api/users/currentuser')
+
+  return response.data;
+}
+
+export default LandingPage;
