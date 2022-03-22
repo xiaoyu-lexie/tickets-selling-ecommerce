91f8fe7a10607f106ede52652e47c515751d0b06
Lexie Chen <xiaoyuchen.uncc@gmail.com>
2022-03-21 18:37:35 -0700

    passing through cookie

--- client/pages/index.js
+++ client/pages/index.js
@@ -7,14 +7,12 @@ const LandingPage = ({currentuser}) => {
   return <h1>Landing page</h1>
 };
 
-LandingPage.getInitialProps = async () => {
+LandingPage.getInitialProps = async ({req}) => {
   if (typeof window === 'undefined') {
     // we are on the server
     const {data} = await axios.get(
       'http://ingress-nginx-controller.ingress-nginx.svc.cluster.local/api/users/currentuser', {
-        headers: {
-          Host: 'ticketing.dev'
-        }
+        headers: req.headers
       }
     );
     return data;
