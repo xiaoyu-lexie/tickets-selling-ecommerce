164191943669e9cc3ada8230ce5272f621a59050
Lexie Chen <xiaoyuchen.uncc@gmail.com>
2022-03-22 17:00:47 -0700

    create build-client helper function

--- client/pages/index.js
+++ client/pages/index.js
@@ -1,29 +1,15 @@
-import axios from 'axios';
+import buildClient from '../api/build-client';
 
-const LandingPage = ({currentuser}) => {
-  console.log(currentuser);
+const LandingPage = ({currentUser}) => {
+  console.log(currentUser);
   // axios.get('/api/users/currentuser')
 
   return <h1>Landing page</h1>
 };
 
-LandingPage.getInitialProps = async ({req}) => {
-  if (typeof window === 'undefined') {
-    // we are on the server
-    const {data} = await axios.get(
-      'http://ingress-nginx-controller.ingress-nginx.svc.cluster.local/api/users/currentuser', {
-        headers: req.headers
-      }
-    );
-    return data;
-  } else {
-    //we are on the client(browser)
-    const {data} = await axios.get('/api/users/currentuser')
-    return data;
-  }
-
-
-  return {}
+LandingPage.getInitialProps = async (context) => {
+  const {data} = await buildClient(context).get('/api/users/currentuser');
+  return data;
 }
 
 export default LandingPage;
