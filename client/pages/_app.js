6bde72ef14140fead828216f0110137e12a840bc
Lexie Chen <xiaoyuchen.uncc@gmail.com>
2022-03-23 15:35:11 -0700

    handle multiple getInitialProps

--- client/pages/_app.js
+++ client/pages/_app.js
@@ -1,5 +1,24 @@
 import 'bootstrap/dist/css/bootstrap.css';
+import buildClient from '../api/build-client';
 
-export default ({ Component, pageProps}) => {
-  return <Component {...pageProps} />
-}
+const AppComponent = ({ Component, pageProps}) => {
+  return (
+    <div>
+      <h1>Headers!!!</h1>
+      <Component {...pageProps} />
+    </div>
+  )
+};
+
+AppComponent.getInitialProps = async(appContext) => {
+  const {data} = await buildClient(appContext.ctx).get('/api/users/currentuser');
+
+  let pageProps = {};
+  if (appContext.Component.getInitialProps) {
+    pageProps = await appContext.Component.getInitialProps(appContext.ctx)
+  };
+
+  return data;
+};
+
+export default AppComponent;
