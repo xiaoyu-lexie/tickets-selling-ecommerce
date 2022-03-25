bb8f919bb44be40a8b77093dc3e88ccca4bcd102
Lexie Chen <xiaoyuchen.uncc@gmail.com>
2022-03-25 14:24:46 -0700

    create sign out

--- client/pages/index.js
+++ client/pages/index.js
@@ -7,7 +7,6 @@ const LandingPage = ({currentUser}) => {
 };
 
 LandingPage.getInitialProps = async (context) => {
-  console.log('LANDING PAGE!!!!')
   const {data} = await buildClient(context).get('/api/users/currentuser');
 
   return data;
